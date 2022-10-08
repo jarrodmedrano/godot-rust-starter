50f817150314673ead43d5fc10f5e2493db2b5d2
Jarrod Medrano <jmedran@gmail.com>
2022-10-08 15:20:22 -0600

    change hello world method

--- src/lib.rs
+++ src/lib.rs
@@ -21,6 +21,20 @@ impl HelloWorld {
 // will generate code to automatically bind any exported methods to Godot.
 #[methods]
 impl HelloWorld {
+    // To make a method known to Godot, use the #[method] attribute.
+    // In Godot, script "classes" do not actually inherit the parent class.
+    // Instead, they are "attached" to the parent object, called the "base".
+    //
+    // If access to the base instance is desired, the 2nd parameter can be
+    // annotated with #[base]. It must have type `&T` or `TRef<T>`, where `T`
+    // is the base type specified in #[inherit]. If you don't need this parameter,
+    // feel free to omit it entirely.
+    #[method]
+    fn _ready(&self, #[base] base: &Node) {
+        // The `godot_print!` macro works like `println!` but prints to the Godot-editor
+        // output tab as well.
+        godot_print!("Hello world from node {}!", base.to_string());
+    }
 }
 // Macro that creates the entry-points of the dynamic library.
 godot_init!(init);
